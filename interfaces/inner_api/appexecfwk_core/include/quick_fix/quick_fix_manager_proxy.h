d88adcf8136ef47c4c0efb678826c4bee764f0b9
wangtiantian <wangtiantian19@huawei.com>
2022-07-18 15:44:22 +0800

    IssueNo:#I5HL8R:add quick fix interface Description:add quick fix interface Sig:SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

---
+++ interfaces/inner_api/appexecfwk_core/include/quick_fix/quick_fix_manager_proxy.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FOUNDATION_BUNDLEMANAGER_BUNDLE_FRAMEWORK_INNERKITS_APPEXECFWK_CORE_INCLUDE_QUICK_FIX_MANAGER_PROXY_H
+#define FOUNDATION_BUNDLEMANAGER_BUNDLE_FRAMEWORK_INNERKITS_APPEXECFWK_CORE_INCLUDE_QUICK_FIX_MANAGER_PROXY_H
+
+#include "quick_fix_manager_interface.h"
+#include "iremote_proxy.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+class QuickFixManagerProxy : public IRemoteProxy<IQuickFixManager> {
+public:
+    explicit QuickFixManagerProxy(const sptr<IRemoteObject>& object);
+    virtual ~QuickFixManagerProxy();
+
+    virtual bool DeployQuickFix(const std::vector<string> &bundleFilePaths,
+        const sptr<IQuickFixStatusCallBack> &statusCallBack) override;
+
+    virtual bool SwitchQuickFix(const std::string &bundleName,
+        const sptr<IQuickFixStatusCallBack> &statusCallBack) override;
+
+    virtual bool DeleteQuickFix(const std::string &bundleName,
+        const sptr<IQuickFixStatusCallBack> &statusCallBack) override;
+
+private:
+    bool SendRequest(IDefaultApp::Message code, MessageParcel &data, MessageParcel &reply);
+    static inline BrokerDelegator<QuickFixManagerProxy> delegator_;
+};
+} // AppExecFwk
+} // OHOS
+#endif // FOUNDATION_BUNDLEMANAGER_BUNDLE_FRAMEWORK_INNERKITS_APPEXECFWK_CORE_INCLUDE_QUICK_FIX_MANAGER_PROXY_H
---
+++ interfaces/inner_api/appexecfwk_core/src/quick_fix/quick_fix_manager_host.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "quick_fix_manager_host.h"
+
+#include "app_log_wrapper.h"
+#include "appexecfwk_errors.h"
+#include "hitrace_meter.h"
+#include "ipc_types.h"
+
+namespace OHOS {
+namespace namespace AppExecFwk {
+QuickFixManagerHost::QuickFixManagerHost()
+{
+    APP_LOGD("create QuickFixManagerHost.");
+}
+
+QuickFixManagerHost::~QuickFixManagerHost()
+{
+    APP_LOGD("destroy QuickFixManagerHost.");
+}
+
+int QuickFixManagerHost::OnRemoteRequest(uint32_t code, MessageParcel& data,
+    MessageParcel& reply, MessageOption& option)
+{
+    APP_LOGI("QuickFixManagerHost OnRemoteRequest, message code : %{public}u", code);
+    std::u16string descriptor = QuickFixManagerHost::GetDescriptor();
+    std::u16string remoteDescriptor = data.ReadInterfaceToken();
+    if (descriptor != remoteDescriptor) {
+        APP_LOGE("descriptor invalid.");
+        return OBJECT_NULL;
+    }
+
+    switch (code) {
+        case IQuickFixManager::Message::DEPLOY_QUICK_FIX:
+            return HandleDeployQuickFix(data, reply);
+        case IQuickFixManager::Message::SWITCH_QUICK_FIX:
+            return HandleSwitchQuickFix(data, reply);
+        case IQuickFixManager::Message::DELETE_QUICK_FIX:
+            return HandleDeleteQuickFix(data, reply);
+        default:
+            APP_LOGW("DefaultAppHost receive unknown code, code = %{public}d", code);
+            return IPCObjectStub::OnRemoteRequest(code, data, reply, option);
+    }
+}
+
+ErrCode QuickFixManagerHost::HandleDeployQuickFix(MessageParcel& data, MessageParcel& reply)
+{
+    APP_LOGI("begin to HandleDeployQuickFix.");
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
+    std::vector<std::string> bundleFilePaths;
+    if (data.ReadStringVector(&bundleFilePaths)) {
+        APP_LOGE("read bundleFilePatchs failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    sptr<IRemoteObject> object = data.ReadObject<IRemoteObject>();
+    if (object == nullptr) {
+        APP_LOGE("read statusCallBack failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    sptr<IQuickFixStatusCallBack> statusCallBack = iface_cast<IQuickFixStatusCallBack>(object);
+
+    bool ret = DeployQuickFix(bundleFilePaths, statusCallBack);
+    if (!reply.WriteBool(ret)) {
+        APP_LOGE("write ret failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    return ERR_OK;
+}
+
+ErrCode QuickFixManagerHost::HandleSwitchQuickFix(MessageParcel& data, MessageParcel& reply)
+{
+    APP_LOGI("begin to HandleSwitchQuickFix.");
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
+    std::string bundleName = data.ReadString();
+    sptr<IRemoteObject> object = data.ReadObject<IRemoteObject>();
+    if (object == nullptr) {
+        APP_LOGE("read statusCallBack failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    sptr<IQuickFixStatusCallBack> statusCallBack = iface_cast<IQuickFixStatusCallBack>(object);
+
+    bool ret = SwitchQuickFix(bundleName, statusCallBack);
+    if (!reply.WriteBool(ret)) {
+        APP_LOGE("write ret failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    return ERR_OK;
+}
+
+ErrCode QuickFixManagerHost::HandleDeleteQuickFix(MessageParcel& data, MessageParcel& reply)
+{
+    APP_LOGI("begin to HandleDeleteQuickFix.");
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
+    std::string bundleName = data.ReadString();
+    sptr<IRemoteObject> object = data.ReadObject<IRemoteObject>();
+    if (object == nullptr) {
+        APP_LOGE("read statusCallBack failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    sptr<IQuickFixStatusCallBack> statusCallBack = iface_cast<IQuickFixStatusCallBack>(object);
+
+    bool ret = DeployQuickFix(bundleFilePaths, statusCallBack);
+    if (!reply.WriteBool(ret)) {
+        APP_LOGE("write ret failed.");
+        return ERR_APPEXECFWK_PARCEL_ERROR;
+    }
+    return ERR_OK;
+}
+} // AppExecFwk
+} // namespace OHOS
