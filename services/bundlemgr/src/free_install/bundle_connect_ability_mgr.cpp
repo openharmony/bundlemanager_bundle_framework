337a62470a0e719d85c5a4b8a4ed6229ce40e898
zhaogan <zhaogan2@huawei.com>
2023-03-06 21:04:58 +0800

    Issue: #I6KDE4 Description:修改免安装接口QueryAbilityInfo的权限校验 Sig: SIG_ApplicaitonFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: zhaogan <zhaogan2@huawei.com>

--- services/bundlemgr/src/free_install/bundle_connect_ability_mgr.cpp
+++ services/bundlemgr/src/free_install/bundle_connect_ability_mgr.cpp
@@ -778,8 +778,7 @@ void BundleConnectAbilityMgr::GetTargetAbilityInfo(const Want &want, int32_t use
         std::string value = wantParams.GetStringByType(info, typeId);
         extValues.emplace(it.first, value);
     }
-    int32_t callingUid = IPCSkeleton::GetCallingUid();
-    APP_LOGD("callingUid: %{public}d", callingUid);
+    auto callingUid = want.GetIntParam(PARAM_FREEINSTALL_UID, IPCSkeleton::GetCallingUid());
 
     targetAbilityInfo->targetExtSetting.extValues = extValues;
     targetAbilityInfo->targetInfo.transactId = std::to_string(this->GetTransactId());
@@ -869,7 +868,6 @@ bool BundleConnectAbilityMgr::CheckIsModuleNeedUpdate(
         targetAbilityInfo->targetExtSetting = *targetExtSetting;
         targetAbilityInfo->version = DEFAULT_VERSION;
         this->GetTargetAbilityInfo(want, userId, innerBundleInfo, targetAbilityInfo);
-        targetAbilityInfo->targetInfo.callingUid = want.GetIntParam(PARAM_FREEINSTALL_UID, IPCSkeleton::GetCallingUid());
         if (targetAbilityInfo->targetInfo.moduleName.empty()) {
             targetAbilityInfo->targetInfo.moduleName = moduleName;
         }
@@ -974,7 +972,6 @@ bool BundleConnectAbilityMgr::QueryAbilityInfo(const Want &want, int32_t flags,
     targetAbilityInfo->targetExtSetting = *targetExtSetting;
     targetAbilityInfo->version = DEFAULT_VERSION;
     this->GetTargetAbilityInfo(want, userId, innerBundleInfo, targetAbilityInfo);
-    targetAbilityInfo->targetInfo.callingUid = want.GetIntParam(PARAM_FREEINSTALL_UID, IPCSkeleton::GetCallingUid());
     sptr<FreeInstallParams> freeInstallParams = new(std::nothrow) FreeInstallParams();
     if (freeInstallParams == nullptr) {
         APP_LOGE("freeInstallParams is nullptr");
@@ -1013,6 +1010,13 @@ bool BundleConnectAbilityMgr::SilentInstall(const Want &want, int32_t userId, co
     targetAbilityInfo->version = DEFAULT_VERSION;
     InnerBundleInfo innerBundleInfo;
     GetTargetAbilityInfo(want, userId, innerBundleInfo, targetAbilityInfo);
+    auto callingUid = IPCSkeleton::GetCallingUid();
+    std::vector<std::string> callingBundleNames;
+    std::vector<std::string> callingAppids;
+    GetCallingInfo(userId, callingUid, callingBundleNames, callingAppids);
+    targetAbilityInfo->targetInfo.callingUid = callingUid;
+    targetAbilityInfo->targetInfo.callingBundleNames = callingBundleNames;
+    targetAbilityInfo->targetInfo.callingAppIds = callingAppids;
     sptr<FreeInstallParams> freeInstallParams = new(std::nothrow) FreeInstallParams();
     if (freeInstallParams == nullptr) {
         APP_LOGE("freeInstallParams is nullptr");
@@ -1063,7 +1067,6 @@ void BundleConnectAbilityMgr::UpgradeAtomicService(const Want &want, int32_t use
     targetAbilityInfo->targetExtSetting = *targetExtSetting;
     targetAbilityInfo->version = DEFAULT_VERSION;
     this->GetTargetAbilityInfo(want, userId, innerBundleInfo, targetAbilityInfo);
-    targetAbilityInfo->targetInfo.callingUid = want.GetIntParam(PARAM_FREEINSTALL_UID, IPCSkeleton::GetCallingUid());
     if (targetAbilityInfo->targetInfo.moduleName.empty()) {
         auto baseAbilitiesInfo = innerBundleInfo.GetInnerAbilityInfos();
         for (const auto& info : baseAbilitiesInfo) {
