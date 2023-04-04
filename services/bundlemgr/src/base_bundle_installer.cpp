672652064a7ad5cf553d0e8869a68fa4ddf94408
Lotol <linxiangzhi@huawei.com>
2023-04-04 09:00:45 +0000

    update services/bundlemgr/src/base_bundle_installer.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- services/bundlemgr/src/base_bundle_installer.cpp
+++ services/bundlemgr/src/base_bundle_installer.cpp
@@ -1676,10 +1676,12 @@ ErrCode BaseBundleInstaller::ProcessModuleUpdate(InnerBundleInfo &newInfo,
         }
     }
 #ifdef BUNDLE_FRAMEWORK_OVERLAY_INSTALLATION
-    result = OverlayDataMgr::GetInstance()->RemoveOverlayModuleConnection(newInfo, oldInfo);
-    if ((newInfo.GetOverlayType() != NON_OVERLAY_TYPE) && (result != ERR_OK)) {
-        APP_LOGE("remove overlay connection failed due to %{public}d", result);
-        return result;
+    if (newInfo.GetOverlayType() != NON_OVERLAY_TYPE) {
+        result = OverlayDataMgr::GetInstance()->RemoveOverlayModuleConnection(newInfo, oldInfo);
+        if (result != ERR_OK) {
+            APP_LOGE("remove overlay connection failed due to %{public}d", result);
+            return result;
+        }
     }
     // stage model to FA model
     if (!newInfo.GetIsNewVersion() && oldInfo.GetIsNewVersion()) {
