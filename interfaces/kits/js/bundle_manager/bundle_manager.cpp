cb509fd79677241973e1ad7757b5ef6be82456a1
wuhong <wuhong4@huawei.com>
2023-07-23 16:17:25 +0800

    IssueNo:#I7LWJ2:change sysDialog/atomicServicePanel type to sysDialog/atomicServicePanel in module.json5 Description:add sysDialog/atomicServicePanel type Sig:SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: 吴鸿 <wh584221@163.com>

--- interfaces/kits/js/bundle_manager/bundle_manager.cpp
+++ interfaces/kits/js/bundle_manager/bundle_manager.cpp
@@ -2236,9 +2236,10 @@ void CreateExtensionAbilityTypeObject(napi_env env, napi_value value)
     NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "SYSDIALOG_COMMON", nSysDialogCommon));
 
     napi_value nSysDialogAtomicServicePanel;
+    NAPI_CALL_RETURN_VOID(env,napi_create_int32(env,
+        static_cast<int32_t>(ExtensionAbilityType::SYSDIALOG_ATOMICSERVICEPANEL), &nSysDialogAtomicServicePanel));
     NAPI_CALL_RETURN_VOID(env,
-        napi_create_int32(env, static_cast<int32_t>(ExtensionAbilityType::SYSDIALOG_ATOMICSERVICEPANEL), &nSysDialogAtomicServicePanel));
-    NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "SYSDIALOG_ATOMICSERVICEPANEL", nSysDialogAtomicServicePanel));
+        napi_set_named_property(env, value, "SYSDIALOG_ATOMICSERVICEPANEL", nSysDialogAtomicServicePanel));
 }
 
 void CreateApplicationFlagObject(napi_env env, napi_value value)
