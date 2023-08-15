e6b457761c31b973a253704021f40af9fb81b0e2
Lotol <linxiangzhi@huawei.com>
2023-08-15 06:35:32 +0000

    update test/fuzztest/bundleinstalldhost_fuzzer/bundleinstalldhost_fuzzer.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- test/fuzztest/bundleinstalldhost_fuzzer/bundleinstalldhost_fuzzer.cpp
+++ test/fuzztest/bundleinstalldhost_fuzzer/bundleinstalldhost_fuzzer.cpp
@@ -27,7 +27,7 @@ namespace OHOS {
 constexpr size_t FOO_MAX_LEN = 1024;
 constexpr size_t U32_AT_SIZE = 4;
 constexpr size_t MESSAGE_SIZE = 24;
-ststatic InstalldHost installdHost;
+static InstalldHost installdHost;
 
 uint32_t GetU32Data(const char* ptr)
 {
