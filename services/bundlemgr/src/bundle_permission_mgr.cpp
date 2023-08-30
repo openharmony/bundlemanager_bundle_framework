29517d62c623cfc27d5ff3a235aecab6f32ac970
zhaogan <zhaogan2@huawei.com>
2023-08-30 17:34:15 +0800

    Issue: #I7X8QK Description:预加载适配合一桌面2 Sig: SIG_ApplicaitonFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: zhaogan <zhaogan2@huawei.com>

--- services/bundlemgr/src/bundle_permission_mgr.cpp
+++ services/bundlemgr/src/bundle_permission_mgr.cpp
@@ -793,7 +793,7 @@ bool BundlePermissionMgr::VerifyPreload(const AAFwk::Want &want)
         return false;
     }
     std::string bundleName = want.GetElement().GetBundleName();
-    return bundleName == callingBundleName || bundleName == SCENEBOARD_BUNDLE_NAME;
+    return bundleName == callingBundleName || callingBundleName == SCENEBOARD_BUNDLE_NAME;
 }
 
 bool BundlePermissionMgr::InnerUpdateDefinePermission(
