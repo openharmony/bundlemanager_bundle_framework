7c65b832e7fb4954e6955cef277a9a3b1fdf6185
zhaogan <zhaogan2@huawei.com>
2023-09-04 10:58:03 +0800

    Issue: #I7WQSB:[新需求]: 首帧绘制结束后执行预加载(回合monthly) Description:预加载适配合一桌面 Sig: SIG_ApplicaitonFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: zhaogan <zhaogan2@huawei.com>

--- services/bundlemgr/src/bundle_permission_mgr.cpp
+++ services/bundlemgr/src/bundle_permission_mgr.cpp
@@ -28,6 +28,7 @@ namespace AppExecFwk {
 namespace {
 // pre bundle profile
 constexpr const char* INSTALL_LIST_PERMISSIONS_CONFIG = "/etc/app/install_list_permissions.json";
+constexpr const char* SCENEBOARD_BUNDLE_NAME = "com.ohos.sceneboard";
 }
 
 using namespace OHOS::Security;
@@ -796,7 +797,7 @@ bool BundlePermissionMgr::VerifyPreload(const AAFwk::Want &want)
         return false;
     }
     std::string bundleName = want.GetElement().GetBundleName();
-    return bundleName == callingBundleName;
+    return bundleName == callingBundleName || callingBundleName == SCENEBOARD_BUNDLE_NAME;
 }
 
 bool BundlePermissionMgr::InnerUpdateDefinePermission(
