a0a522e99556b119110fb1ab684a7fa6e3fa7e82
wangtiantian <wangtiantian19@huawei.com>
2023-10-06 18:24:33 +0800

    IssueNo:#I85LFI Description:add testcase Sig:SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

---
+++ services/bundlemgr/test/unittest/bms_bundle_resource_test/bms_bundle_resource_test.cpp
@@ -0,0 +1,408 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fstream>
+#include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+
+#include "application_info.h"
+#include "bundle_info.h"
+#include "bundle_installer_host.h"
+#include "bundle_mgr_service.h"
+#include "bundle_permission_mgr.h"
+
+#ifdef BUNDLE_FRAMEWORK_BUNDLE_RESOURCE
+#include "bundle_resource_rdb.h"
+#include "bundle_system_state.h"
+#endif
+
+#include "bundle_verify_mgr.h"
+#include "inner_bundle_info.h"
+#include "installd/installd_service.h"
+#include "installd_client.h"
+#include "mock_status_receiver.h"
+#include "parameter.h"
+#include "permission_define.h"
+#include "remote_ability_info.h"
+#include "scope_guard.h"
+
+using namespace testing::ext;
+using namespace std::chrono_literals;
+using namespace OHOS;
+using namespace OHOS::AppExecFwk;
+using namespace OHOS::Security;
+
+namespace OHOS {
+namespace {
+const int32_t USERID = 100;
+const int32_t WAIT_TIME = 5; // init mocked bms
+}  // namespace
+
+class BmsBundleResourceTest : public testing::Test {
+public:
+    BmsBundleResourceTest();
+    ~BmsBundleResourceTest();
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+    ErrCode InstallBundle(const std::string &bundlePath) const;
+    ErrCode UpdateBundle(const std::string &bundlePath) const;
+    ErrCode UnInstallBundle(const std::string &bundleName) const;
+    const std::shared_ptr<BundleDataMgr> GetBundleDataMgr() const;
+    void StartInstalldService() const;
+    void StartBundleService();
+
+private:
+    static std::shared_ptr<InstalldService> installdService_;
+    static std::shared_ptr<BundleMgrService> bundleMgrService_;
+};
+
+std::shared_ptr<BundleMgrService> BmsBundleResourceTest::bundleMgrService_ =
+    DelayedSingleton<BundleMgrService>::GetInstance();
+
+std::shared_ptr<InstalldService> BmsBundleResourceTest::installdService_ =
+    std::make_shared<InstalldService>();
+
+BmsBundleResourceTest::BmsBundleResourceTest()
+{}
+
+BmsBundleResourceTest::~BmsBundleResourceTest()
+{}
+
+void BmsBundleResourceTest::SetUpTestCase()
+{}
+
+void BmsBundleResourceTest::TearDownTestCase()
+{
+    bundleMgrService_->OnStop();
+}
+
+void BmsBundleResourceTest::SetUp()
+{
+    StartInstalldService();
+    StartBundleService();
+}
+
+void BmsBundleResourceTest::TearDown()
+{}
+
+ErrCode BmsBundleResourceTest::InstallBundle(const std::string &bundlePath) const
+{
+    if (!bundleMgrService_) {
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    auto installer = bundleMgrService_->GetBundleInstaller();
+    if (!installer) {
+        EXPECT_FALSE(true) << "the installer is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    sptr<MockStatusReceiver> receiver = new (std::nothrow) MockStatusReceiver();
+    if (!receiver) {
+        EXPECT_FALSE(true) << "the receiver is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    InstallParam installParam;
+    installParam.installFlag = InstallFlag::NORMAL;
+    installParam.userId = USERID;
+    bool result = installer->Install(bundlePath, installParam, receiver);
+    EXPECT_TRUE(result);
+    return receiver->GetResultCode();
+}
+
+ErrCode BmsBundleResourceTest::UpdateBundle(const std::string &bundlePath) const
+{
+    if (!bundleMgrService_) {
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    auto installer = bundleMgrService_->GetBundleInstaller();
+    if (!installer) {
+        EXPECT_FALSE(true) << "the installer is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    sptr<MockStatusReceiver> receiver = new (std::nothrow) MockStatusReceiver();
+    if (!receiver) {
+        EXPECT_FALSE(true) << "the receiver is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    InstallParam installParam;
+    installParam.installFlag = InstallFlag::REPLACE_EXISTING;
+    installParam.userId = USERID;
+    bool result = installer->Install(bundlePath, installParam, receiver);
+    EXPECT_TRUE(result);
+    return receiver->GetResultCode();
+}
+
+ErrCode BmsBundleResourceTest::UnInstallBundle(const std::string &bundleName) const
+{
+    if (!bundleMgrService_) {
+        return ERR_APPEXECFWK_UNINSTALL_BUNDLE_MGR_SERVICE_ERROR;
+    }
+    auto installer = bundleMgrService_->GetBundleInstaller();
+    if (!installer) {
+        EXPECT_FALSE(true) << "the installer is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    sptr<MockStatusReceiver> receiver = new (std::nothrow) MockStatusReceiver();
+    if (!receiver) {
+        EXPECT_FALSE(true) << "the receiver is nullptr";
+        return ERR_APPEXECFWK_INSTALL_INTERNAL_ERROR;
+    }
+    InstallParam installParam;
+    installParam.installFlag = InstallFlag::NORMAL;
+    installParam.userId = USERID;
+    bool result = installer->Uninstall(bundleName, installParam, receiver);
+    EXPECT_TRUE(result);
+    return receiver->GetResultCode();
+}
+
+void BmsBundleResourceTest::StartInstalldService() const
+{
+    if (!installdService_->IsServiceReady()) {
+        installdService_->Start();
+    }
+}
+
+void BmsBundleResourceTest::StartBundleService()
+{
+    if (!bundleMgrService_->IsServiceReady()) {
+        bundleMgrService_->OnStart();
+        std::this_thread::sleep_for(std::chrono::seconds(WAIT_TIME));
+    }
+}
+
+const std::shared_ptr<BundleDataMgr> BmsBundleResourceTest::GetBundleDataMgr() const
+{
+    return bundleMgrService_->GetDataMgr();
+}
+
+#ifdef BUNDLE_FRAMEWORK_BUNDLE_RESOURCE
+/**
+ * @tc.number: BmsBundleResourceTest_0001
+ * Function: GetKey
+ * @tc.name: test GetKey
+ * @tc.desc: 1. system running normally
+ *           2. test ResourceInfo GetKey
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0001, Function | SmallTest | Level0)
+{
+    ResourceInfo resourceInfo;
+    resourceInfo.bundleName_ = "bundleName";
+    std::string key = resourceInfo.GetKey();
+    EXPECT_EQ(key, "bundleName");
+
+    resourceInfo.moduleName_ = "moduleName";
+    resourceInfo.abilityName_ = "abilityName";
+    key = resourceInfo.GetKey();
+    EXPECT_EQ(key, "bundleName/moduleName/abilityName");
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0002
+ * Function: ParseKey
+ * @tc.name: test ParseKey
+ * @tc.desc: 1. system running normally
+ *           2. test ResourceInfo ParseKey
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0002, Function | SmallTest | Level0)
+{
+    std::string key = "bundleName";
+    ResourceInfo resourceInfo;
+    resourceInfo.ParseKey(key);
+    EXPECT_EQ(resourceInfo.bundleName_, "bundleName");
+    EXPECT_EQ(resourceInfo.moduleName_, "");
+    EXPECT_EQ(resourceInfo.abilityName_, "");
+
+    key = "bundleName/";
+    resourceInfo.ParseKey(key);
+    EXPECT_EQ(resourceInfo.bundleName_, "bundleName");
+    EXPECT_EQ(resourceInfo.moduleName_, "");
+    EXPECT_EQ(resourceInfo.abilityName_, "");
+
+    key = "bundleName/abilityName";
+    resourceInfo.ParseKey(key);
+    EXPECT_EQ(resourceInfo.bundleName_, "bundleName");
+    EXPECT_EQ(resourceInfo.moduleName_, "");
+    EXPECT_EQ(resourceInfo.abilityName_, "abilityName");
+
+    key = "bundleName/moduleName/abilityName";
+    resourceInfo.ParseKey(key);
+    EXPECT_EQ(resourceInfo.bundleName_, "bundleName");
+    EXPECT_EQ(resourceInfo.moduleName_, "moduleName");
+    EXPECT_EQ(resourceInfo.abilityName_, "abilityName");
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0003
+ * Function: GetKey ParseKey
+ * @tc.name: test GetKey and ParseKey
+ * @tc.desc: 1. system running normally
+ *           2. test ResourceInfo GetKey and ParseKey
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0003, Function | SmallTest | Level0)
+{
+    ResourceInfo resourceInfo;
+    resourceInfo.bundleName_ = "bundleName";
+    resourceInfo.moduleName_ = "moduleName";
+    resourceInfo.abilityName_ = "abilityName";
+    std::string key = resourceInfo.GetKey();
+
+    ResourceInfo newResourceInfo;
+    newResourceInfo.ParseKey(key);
+
+    EXPECT_EQ(resourceInfo.bundleName_, newResourceInfo.bundleName_);
+    EXPECT_EQ(resourceInfo.moduleName_, newResourceInfo.moduleName_);
+    EXPECT_EQ(resourceInfo.abilityName_, newResourceInfo.abilityName_);
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0004
+ * Function: BundleSystemState
+ * @tc.name: test BundleSystemState
+ * @tc.desc: 1. system running normally
+ *           2. test BundleSystemState
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0004, Function | SmallTest | Level0)
+{
+    std::string oldColorMode = BundleSystemState::GetInstance().GetSystemColorMode();
+    std::string oldLanguage = BundleSystemState::GetInstance().GetSystemLanguage();
+
+    std::string colorMode = "light";
+    BundleSystemState::GetInstance().SetSystemColorMode(colorMode);
+    EXPECT_EQ(colorMode, BundleSystemState::GetInstance().GetSystemColorMode());
+    BundleSystemState::GetInstance().SetSystemColorMode(oldColorMode);
+
+    std::string language = "zh-Hans";
+    BundleSystemState::GetInstance().SetSystemLanguage(language);
+    EXPECT_EQ(language, BundleSystemState::GetInstance().GetSystemLanguage());
+    BundleSystemState::GetInstance().SetSystemLanguage(oldLanguage);
+
+    std::string key = BundleSystemState::GetInstance().ToString();
+    EXPECT_NE(key, "");
+    bool ans = BundleSystemState::GetInstance().FromString("{");
+    EXPECT_FALSE(ans);
+    ans = BundleSystemState::GetInstance().FromString(key);
+    EXPECT_TRUE(ans);
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0005
+ * Function: BundleResourceRdb
+ * @tc.name: test BundleResourceRdb
+ * @tc.desc: 1. system running normally
+ *           2. test AddResourceInfo
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0005, Function | SmallTest | Level0)
+{
+    BundleResourceRdb resourceRdb;
+    ResourceInfo resourceInfo;
+    bool ans = resourceRdb.AddResourceInfo(resourceInfo);
+    EXPECT_FALSE(ans);
+    ans = resourceRdb.DeleteResourceInfo(resourceInfo.GetKey());
+    EXPECT_FALSE(ans);
+
+    resourceInfo.bundleName_ = "bundleName";
+    ans = resourceRdb.AddResourceInfo(resourceInfo);
+    EXPECT_TRUE(ans);
+    ans = resourceRdb.DeleteResourceInfo(resourceInfo.GetKey());
+    EXPECT_TRUE(ans);
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0006
+ * Function: BundleResourceRdb
+ * @tc.name: test BundleResourceRdb
+ * @tc.desc: 1. system running normally
+ *           2. test AddResourceInfos
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0006, Function | SmallTest | Level0)
+{
+    BundleResourceRdb resourceRdb;
+    std::vector<ResourceInfo> resourceInfos;
+    bool ans = resourceRdb.AddResourceInfos(resourceInfos);
+    EXPECT_FALSE(ans);
+
+    ResourceInfo resourceInfo;
+    resourceInfo.bundleName_ = "bundleName";
+    resourceInfos.push_back(resourceInfo);
+
+    ans = resourceRdb.AddResourceInfos(resourceInfos);
+    EXPECT_TRUE(ans);
+    ans = resourceRdb.DeleteResourceInfo(resourceInfo.GetKey());
+    EXPECT_TRUE(ans);
+
+    resourceInfos.clear();
+    ResourceInfo resourceInfo_2;
+    resourceInfos.push_back(resourceInfo_2);
+    ans = resourceRdb.AddResourceInfos(resourceInfos);
+    EXPECT_FALSE(ans);
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0007
+ * Function: BundleResourceRdb
+ * @tc.name: test BundleResourceRdb
+ * @tc.desc: 1. system running normally
+ *           2. test GetAllResourceName
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0007, Function | SmallTest | Level0)
+{
+    BundleResourceRdb resourceRdb;
+
+    ResourceInfo resourceInfo;
+    resourceInfo.bundleName_ = "bundleName";
+
+    bool ans = resourceRdb.AddResourceInfo(resourceInfo);
+    EXPECT_TRUE(ans);
+
+    std::vector<std::string> keyNames;
+    ans = resourceRdb.GetAllResourceName(keyNames);
+    EXPECT_TRUE(ans);
+    EXPECT_TRUE(std::find(keyNames.begin(), keyNames.end(), resourceInfo.GetKey()) != keyNames.end());
+
+    ans = resourceRdb.DeleteResourceInfo(resourceInfo.GetKey());
+    EXPECT_TRUE(ans);
+}
+
+/**
+ * @tc.number: BmsBundleResourceTest_0008
+ * Function: BundleResourceRdb
+ * @tc.name: test BundleResourceRdb
+ * @tc.desc: 1. system running normally
+ *           2. test IsCurrentColorModeExist
+ */
+HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0008, Function | SmallTest | Level0)
+{
+    BundleResourceRdb resourceRdb;
+    ResourceInfo resourceInfo;
+    resourceInfo.bundleName_ = "bundleName";
+    bool ans = resourceRdb.AddResourceInfo(resourceInfo);
+    EXPECT_TRUE(ans);
+
+    ans = resourceRdb.IsCurrentColorModeExist();
+    EXPECT_TRUE(ans);
+
+    std::string oldColorMode = BundleSystemState::GetInstance().GetSystemColorMode();
+    BundleSystemState::GetInstance().SetSystemColorMode("aaaaaa");
+    ans = resourceRdb.IsCurrentColorModeExist();
+    EXPECT_FALSE(ans);
+    BundleSystemState::GetInstance().SetSystemColorMode(oldColorMode);
+
+    ans = resourceRdb.DeleteResourceInfo(resourceInfo.GetKey());
+    EXPECT_TRUE(ans);
+}
+#endif
+} // OHOS
