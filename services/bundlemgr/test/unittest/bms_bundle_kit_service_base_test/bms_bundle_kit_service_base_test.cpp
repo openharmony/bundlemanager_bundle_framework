7edc7ace1b7edb35d6486ecbbbc1949df932f96b
Lotol <linxiangzhi@huawei.com>
2023-10-10 10:27:38 +0000

    update bms_bundle_kit_service_base_test.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_kit_service_base_test/bms_bundle_kit_service_base_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_kit_service_base_test/bms_bundle_kit_service_base_test.cpp
@@ -149,7 +149,7 @@ void BmsBundleKitServiceBaseTest::SetUp()
     if (!bundleMgrService_->IsServiceReady()) {
         bundleMgrService_->OnStart();
         bundleMgrService_->GetDataMgr()->AddUserId(DEFAULT_USERID);
-    } 
+    }
 }
 
 void BmsBundleKitServiceBaseTest::TearDown()
