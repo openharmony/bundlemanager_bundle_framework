4d936a90bce6a27723191551d78fd831035fffe6
Lotol <linxiangzhi@huawei.com>
2023-11-24 01:39:16 +0000

    update bms_bundle_kit_service_base_test.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_kit_service_base_test/bms_bundle_kit_service_base_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_kit_service_base_test/bms_bundle_kit_service_base_test.cpp
@@ -857,11 +857,11 @@ HWTEST_F(BmsBundleKitServiceBaseTest, BundleDistributedManager_1900, Function | 
     setuid(Constants::FOUNDATION_UID);
     ScopeGuard uidGuard([&] { setuid(Constants::ROOT_UID); });
     auto ret = mgr.QueryRpcIdByAbilityToServiceCenter(targetAbilityInfo);
-    #ifdef ARM64
+#ifdef USE_ARM64
+    EXPECT_TRUE(ret);
+#else
     EXPECT_FALSE(ret);
-    #else
-    EXPECT_FALSE(ret);
-    #endif
+#endif
     sleep(1);
 }
 
@@ -883,7 +883,11 @@ HWTEST_F(BmsBundleKitServiceBaseTest, BundleDistributedManager_2000, Function | 
     setuid(Constants::FOUNDATION_UID);
     ScopeGuard uidGuard([&] { setuid(Constants::ROOT_UID); });
     auto ret = mgr.QueryRpcIdByAbilityToServiceCenter(targetAbilityInfo);
+#ifdef USE_ARM64
+    EXPECT_TRUE(ret);
+#else
     EXPECT_FALSE(ret);
+#endif
     sleep(1);
 }
 
@@ -899,7 +903,11 @@ HWTEST_F(BmsBundleKitServiceBaseTest, BundleDistributedManager_2100, Function | 
     setuid(Constants::FOUNDATION_UID);
     ScopeGuard uidGuard([&] { setuid(Constants::ROOT_UID); });
     auto ret = mgr.QueryRpcIdByAbilityToServiceCenter(targetAbilityInfo);
+#ifdef USE_ARM64
+    EXPECT_TRUE(ret);
+#else
     EXPECT_FALSE(ret);
+#endif
     sleep(1);
 }
 
