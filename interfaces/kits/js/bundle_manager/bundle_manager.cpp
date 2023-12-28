cb5d289d63834515da6108c726dcc75506e0a8a8
kevin <zoumeiguang@huawei.com>
2023-12-28 02:17:46 +0000

    update interfaces/kits/js/bundle_manager/bundle_manager.cpp.
    Signed-off-by: kevin <zoumeiguang@huawei.com>

--- interfaces/kits/js/bundle_manager/bundle_manager.cpp
+++ interfaces/kits/js/bundle_manager/bundle_manager.cpp
@@ -2282,12 +2282,6 @@ void CreateExtensionAbilityTypeObject(napi_env env, napi_value value)
     NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "SYSDIALOG_MEETIMEMESSAGE",
         nSysDialogMeetimeMessage));
 
-    napi_value nSysDialogPrint;
-    NAPI_CALL_RETURN_VOID(env, napi_create_int32(env,
-        static_cast<int32_t>(ExtensionAbilityType::SYSDIALOG_PRINT), &nSysDialogPrint));
-    NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "SYSDIALOG_PRINT",
-        nSysDialogPrint));
-
     napi_value nSysPickerMeetimeContact;
     NAPI_CALL_RETURN_VOID(env, napi_create_int32(env,
         static_cast<int32_t>(ExtensionAbilityType::SYSPICKER_MEETIMECONTACT), &nSysPickerMeetimeContact));
