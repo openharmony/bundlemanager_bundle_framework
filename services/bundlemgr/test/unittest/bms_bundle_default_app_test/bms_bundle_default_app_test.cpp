3beb55eaceadb9261ecc1cade560a29c0f809465
SoftSquirrel <xuhao47@huawei.com>
2024-01-25 10:40:42 +0800

    Issue:#I8Y8ND Description:add default email Sig: SIG_ApplicaitonFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: SoftSquirrel <xuhao47@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_default_app_test/bms_bundle_default_app_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_default_app_test/bms_bundle_default_app_test.cpp
@@ -84,6 +84,8 @@ const std::string INVALID_TYPE3 = "/abc";
 const std::string INVALID_TYPE4 = "*/abc";
 const std::string INVALID_TYPE5 = "abc/*";
 const std::string INVALID_TYPE6 = "*/*";
+const std::string EMAIL_ACTION = "ohos.want.action.sendToData";
+const std::string EMAIL_SCHEME = "mailto";
 const nlohmann::json DEFAULT_CONFIG = R"(
 [{
     "bundleName": "bundleName",
@@ -1396,6 +1398,24 @@ HWTEST_F(BmsBundleDefaultAppTest, BmsBundleDefaultApp_6500, Function | SmallTest
     EXPECT_EQ(result, ERR_OK);
     ASSERT_EQ(bundleInfo.abilityInfos.size(), 1);
     EXPECT_EQ(bundleInfo.abilityInfos[0].name, ABILITY_EMAIL);
+
+    ASSERT_NE(bundleMgrService_, nullptr);
+    auto dataMgr = bundleMgrService_->GetDataMgr();
+    ASSERT_NE(dataMgr, nullptr);
+    Want want;
+    want.SetAction(EMAIL_ACTION);
+    want.SetUri(EMAIL_SCHEME + ":test@test.com");
+    int32_t flags = 0;
+    int32_t userId = 100;
+    bool withDefault = true;
+    std::vector<AbilityInfo> abilityInfos;
+    std::vector<ExtensionAbilityInfo> extensionInfos;
+    bool findDefaultApp = false;
+    bool ret = dataMgr->ImplicitQueryInfos(want, flags, userId, withDefault, abilityInfos, extensionInfos, findDefaultApp);
+    EXPECT_EQ(ret, true);
+    EXPECT_EQ(findDefaultApp, true);
+    ASSERT_EQ(abilityInfos.size(), 1);
+    EXPECT_EQ(abilityInfos[0].name, ABILITY_EMAIL);
 }
 
 /**
