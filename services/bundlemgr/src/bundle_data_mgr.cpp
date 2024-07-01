be0b8893c0f2aafd7ed438d41dc40ea3c118ed92
amao <chenpengfei40@huawei.com>
2024-07-01 11:05:44 +0800

    account opt
    Signed-off-by: amao <chenpengfei40@huawei.com>
    Change-Id: I359cf72f1e5a4211432898ea1fba317cde035c8f

--- services/bundlemgr/src/bundle_data_mgr.cpp
+++ services/bundlemgr/src/bundle_data_mgr.cpp
@@ -6512,7 +6512,7 @@ ErrCode BundleDataMgr::GetSpecifiedDistributionType(
         return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
     }
     if (infoItem->second.GetApplicationBundleType() != BundleType::SHARED) {
-        int32_t userId = AccountHelper::GetCurrentActiveUserId();
+        int32_t userId = AccountHelper::GetOsAccountLocalIdFromUid(IPCSkeleton::GetCallingUid());
         int32_t responseUserId = infoItem->second.GetResponseUserId(userId);
         if (responseUserId == Constants::INVALID_USERID) {
             APP_LOGW("bundleName: %{public}s does not exist in current userId", bundleName.c_str());
@@ -6538,7 +6538,7 @@ ErrCode BundleDataMgr::GetAdditionalInfo(
         return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
     }
     if (infoItem->second.GetApplicationBundleType() != BundleType::SHARED) {
-        int32_t userId = AccountHelper::GetCurrentActiveUserId();
+        int32_t userId = AccountHelper::GetOsAccountLocalIdFromUid(IPCSkeleton::GetCallingUid());
         int32_t responseUserId = infoItem->second.GetResponseUserId(userId);
         if (responseUserId == Constants::INVALID_USERID) {
             APP_LOGW("bundleName: %{public}s does not exist in current userId", bundleName.c_str());
@@ -7149,7 +7149,7 @@ ErrCode BundleDataMgr::SetAdditionalInfo(const std::string& bundleName, const st
     }
 
     if (infoItem->second.GetApplicationBundleType() != BundleType::SHARED) {
-        int32_t userId = AccountHelper::GetCurrentActiveUserId();
+        int32_t userId = AccountHelper::GetOsAccountLocalIdFromUid(IPCSkeleton::GetCallingUid());
         int32_t responseUserId = infoItem->second.GetResponseUserId(userId);
         if (responseUserId == Constants::INVALID_USERID) {
             APP_LOGE("BundleName: %{public}s does not exist in current userId", bundleName.c_str());
