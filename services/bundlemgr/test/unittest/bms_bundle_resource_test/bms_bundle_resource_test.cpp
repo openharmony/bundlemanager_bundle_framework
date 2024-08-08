f355df3a8f331e7fa98f8680ec9a3af453098bc4
wangtiantian <wangtiantian19@huawei.com>
2024-08-08 15:13:52 +0800

    IssueNo:#IAIAU3 Description:fix themeId bug Sig:SIG_ApplicaitonFramework Feature or Bugfix:Bugfix Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_resource_test/bms_bundle_resource_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_resource_test/bms_bundle_resource_test.cpp
@@ -3906,7 +3906,7 @@ HWTEST_F(BmsBundleResourceTest, BmsBundleResourceTest_0173, Function | SmallTest
     BundleResourceCallback bundleResourceCallback;
     bool ret = bundleResourceCallback.SetThemeIdForThemeChanged(themeId);
     EXPECT_FALSE(ret);
-    themeId = 1;
+    themeId = 1000;
     ret = bundleResourceCallback.SetThemeIdForThemeChanged(themeId);
     EXPECT_TRUE(ret);
 }
