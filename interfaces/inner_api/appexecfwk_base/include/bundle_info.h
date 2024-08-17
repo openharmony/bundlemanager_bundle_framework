590372813a5765a45e2fefba438231cd4fb9516f
jiangminsen <jiangminsen@huawei.com>
2024-08-17 10:28:44 +0800

    getAllbundleInfo新增过滤clone的flag挑单到5.0release
    Signed-off-by: jiangminsen <jiangminsen@huawei.com>

--- interfaces/inner_api/appexecfwk_base/include/bundle_info.h
+++ interfaces/inner_api/appexecfwk_base/include/bundle_info.h
@@ -63,6 +63,7 @@ enum class GetBundleInfoFlag {
     GET_BUNDLE_INFO_WITH_ROUTER_MAP = 0x00000200,
     GET_BUNDLE_INFO_WITH_SKILL = 0x00000800,
     GET_BUNDLE_INFO_ONLY_WITH_LAUNCHER_ABILITY = 0x00001000,
+    GET_BUNDLE_INFO_EXCLUDE_CLONE = 0x00004000,
 };
 
 struct RequestPermissionUsedScene : public Parcelable {
