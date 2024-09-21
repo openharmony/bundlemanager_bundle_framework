f8191c7b613bdbaacfdd4bf29664df2ce2e659a5
tantingting <tantingting5@huawei.com>
2024-09-21 15:10:36 +0800

    add log
    Signed-off-by: tantingting <tantingting5@huawei.com>

--- services/bundlemgr/src/bundle_profile.cpp
+++ services/bundlemgr/src/bundle_profile.cpp
@@ -2298,6 +2298,7 @@ bool ToInnerBundleInfo(
 
     if (applicationInfo.apiTargetVersion % ServiceConstants::API_VERSION_MOD <=
         ServiceConstants::API_VERSION_THIRTEEN) {
+        APP_LOGD("targetAPIVersion is less than 14, set isCompressNativeLibs flag to true");
         applicationInfo.isCompressNativeLibs = true;
         innerModuleInfo.compressNativeLibs = true;
     }
