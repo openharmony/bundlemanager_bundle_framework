6aae451d92b4aecfec8d0dd4f517ee5c71e96d19
Zhou Shihui <zhoushihui4@huawei.com>
2024-10-09 16:03:13 +0800

    el5目录丢失兜底
    Signed-off-by: Zhou Shihui <zhoushihui4@huawei.com>

--- services/bundlemgr/include/bundle_data_mgr.h
+++ services/bundlemgr/include/bundle_data_mgr.h
@@ -985,6 +985,7 @@ public:
         const std::string &continueBundleName, std::vector<std::string> &bundleNames, int32_t userId);
 
     ErrCode IsBundleInstalled(const std::string &bundleName, int32_t userId, int32_t appIndex, bool &isInstalled);
+    void CreateEl5Dir(const std::vector<CreateDirParam> &el5Params);
 
 private:
     /**
@@ -1070,7 +1071,6 @@ private:
 
     void FilterAbilityInfosByModuleName(const std::string &moduleName, std::vector<AbilityInfo> &abilityInfos) const;
     void CreateGroupDir(const InnerBundleInfo &innerBundleInfo, int32_t userId) const;
-    void CreateEl5Dir(const std::vector<CreateDirParam> &el5Params);
     void InnerCreateEl5Dir(const CreateDirParam &el5Param);
     void SetEl5DirPolicy(const CreateDirParam &el5Param);
 
