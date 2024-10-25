af0f623539ba69d04258d8c3b6c8314ac294c555
sunjiakun <sunjiakun5@huawei.com>
2024-10-25 17:26:54 +0800

    6566 bundlemanager_bundle_framework仓补充TDD
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_installer_test/bms_bundle_installer_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_installer_test/bms_bundle_installer_test.cpp
@@ -6326,4 +6326,21 @@ HWTEST_F(BmsBundleInstallerTest, SetDisposedRuleWhenBundleUpdateStart_0030, Func
     ret = installer.DeleteDisposedRuleWhenBundleUpdateEnd(oldBundleInfo);
     EXPECT_FALSE(ret);
 }
+
+/**
+ * @tc.number: VerifyCodeSignatureForNativeFiles_0100
+ * @tc.name: test VerifyCodeSignatureForNativeFiles
+ * @tc.desc: test VerifyCodeSignatureForNativeFiles of BundleInstallerManager
+*/
+HWTEST_F(BmsBundleInstallerTest, VerifyCodeSignatureForNativeFiles_0100, Function | SmallTest | Level1)
+{
+    AppServiceFwkInstaller appServiceFwkInstaller;
+
+    std::string bundlePath;
+    std::string cpuAbi;
+    std::string targetSoPath;
+    ErrCode ret = appServiceFwkInstaller.VerifyCodeSignatureForNativeFiles(bundlePath, cpuAbi, targetSoPath);
+
+    EXPECT_EQ(ret, ERR_APPEXECFWK_INSTALLD_PARAM_ERROR);
+}
 } // OHOS
