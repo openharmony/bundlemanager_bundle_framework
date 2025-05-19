e3bf4534cac022e0684ef7e2d292d54a2cc64b18
lanhaoyu <lanhaoyu3@huawei-partners.com>
2025-05-19 10:15:54 +0800

    add field
    Signed-off-by: lanhaoyu <lanhaoyu3@huawei-partners.com>

--- services/bundlemgr/test/unittest/bms_data_mgr_test/bms_data_mgr_test.cpp
+++ services/bundlemgr/test/unittest/bms_data_mgr_test/bms_data_mgr_test.cpp
@@ -5446,4 +5446,46 @@ HWTEST_F(BmsDataMgrTest, DeleteShortcutVisibleInfo_0002, Function | MediumTest |
     auto ret = bundleDataMgr.DeleteShortcutVisibleInfo(bundleName, userId, appIndex);
     EXPECT_EQ(ret, ERR_APPEXECFWK_DB_DELETE_ERROR);
 }
+
+/**
+ * @tc.number: GetShortcutVisibleStatus_0001
+ * @tc.name: GetShortcutVisibleStatus
+ * @tc.desc: test GetShortcutVisibleStatus(
+ *     const std::string &bundleName, const std::string &shortcutId, int32_t userId, int32_t appIndex) const
+ */
+HWTEST_F(BmsDataMgrTest, GetShortcutVisibleStatus_0001, Function | MediumTest | Level1)
+{
+    std::shared_ptr<ShortcutVisibleDataStorageRdb> shortcutVisibleDataStorageRdb =
+        std::make_shared<ShortcutVisibleDataStorageRdb>();
+    ASSERT_NE(shortcutVisibleDataStorageRdb, nullptr);
+    ShortcutInfo shortcutInfo;
+    shortcutInfo.bundleName = "";
+    shortcutInfo.id = "";
+    shortcutInfo.visible = true;
+    int32_t appIndex = 0;
+    auto ret = shortcutVisibleDataStorageRdb->GetShortcutVisibleStatus(USERID, appIndex, shortcutInfo);
+    EXPECT_EQ(ret, ERR_BUNDLE_MANAGER_PARAM_ERROR);
+    EXPECT_EQ(shortcutInfo.visible, true);
+}
+
+/**
+ * @tc.number: GetShortcutVisibleStatus_0002
+ * @tc.name: GetShortcutVisibleStatus
+ * @tc.desc: test GetShortcutVisibleStatus(
+ *     const std::string &bundleName, const std::string &shortcutId, int32_t userId, int32_t appIndex) const
+ */
+HWTEST_F(BmsDataMgrTest, GetShortcutVisibleStatus_0002, Function | MediumTest | Level1)
+{
+    std::shared_ptr<ShortcutVisibleDataStorageRdb> shortcutVisibleDataStorageRdb =
+        std::make_shared<ShortcutVisibleDataStorageRdb>();
+    ASSERT_NE(shortcutVisibleDataStorageRdb, nullptr);
+    ShortcutInfo shortcutInfo;
+    shortcutInfo.bundleName = "bundleName";
+    shortcutInfo.id = "shortcutId";
+    shortcutInfo.visible = false;
+    int32_t appIndex = 0;
+    auto ret = shortcutVisibleDataStorageRdb->GetShortcutVisibleStatus(USERID, appIndex, shortcutInfo);
+    EXPECT_EQ(ret, ERR_OK);
+    EXPECT_EQ(shortcutInfo.visible, false);
+}
 } // OHOS
