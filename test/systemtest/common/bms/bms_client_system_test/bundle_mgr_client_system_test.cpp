9bbbf372a14b9b7193f552b949fb7b8417c0d8c4
liuyaoqian <liuyaoqian@huawei.com>
2025-05-29 08:42:02 +0000

    update test/systemtest/common/bms/bms_client_system_test/bundle_mgr_client_system_test.cpp.
    Signed-off-by: liuyaoqian <liuyaoqian@huawei.com>

--- test/systemtest/common/bms/bms_client_system_test/bundle_mgr_client_system_test.cpp
+++ test/systemtest/common/bms/bms_client_system_test/bundle_mgr_client_system_test.cpp
@@ -2333,12 +2333,7 @@ HWTEST_F(BundleMgrClientSystemTest, BundleMgrClientImpl_0012, TestSize.Level1)
     impl.bundleMgr_ = proxy;
     impl.Connect();
     ErrCode res = impl.CreateBundleDataDir(DEFAULT_USERID);
-    auto ret = access("/data/app/el2/0/sharefiles", F_OK);
-    if (ret == 0) {
-        EXPECT_EQ(res, ERR_OK);
-    } else {
-        EXPECT_EQ(res, ERR_APPEXECFWK_INSTALLD_CREATE_DIR_FAILED);
-    }
+    EXPECT_EQ(res, ERR_OK);
     impl.OnDeath();
 }
 
