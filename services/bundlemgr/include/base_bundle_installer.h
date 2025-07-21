f0b29bc1f9c85fe96224d75bc111d371c05f5640
daiyujia <daiyujia@h-partners.com>
2025-07-21 10:10:10 +0800

    wqIssueNo:#ICNKPM Description:应用白名单管控 Sig:bundleManager Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: daiyujia <daiyujia@h-partners.com>
    Change-Id: I321f878c38d1b5497ae9f714180c3fa1a598b347

--- services/bundlemgr/include/base_bundle_installer.h
+++ services/bundlemgr/include/base_bundle_installer.h
@@ -860,6 +860,7 @@ private:
     ErrCode ProcessDynamicIconFileWhenUpdate(const InnerBundleInfo &oldInfo, const std::string &oldPath,
         const std::string &newPath);
     void ProcessUpdateShortcut();
+    ErrCode CheckArkTSMode(const std::unordered_map<std::string, InnerBundleInfo> &newInfos);
 
     bool isAppExist_ = false;
     bool isContainEntry_ = false;
