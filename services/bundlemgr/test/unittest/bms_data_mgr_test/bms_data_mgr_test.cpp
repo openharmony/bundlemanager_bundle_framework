31a7d3b80ccab0addd55783470551f24decf758d
张欣宇 <zhangxinyu74@h-partners.com>
2025-07-21 08:58:36 +0800

    开机阶段appControl与SetAbilityEnabled时序问题修复-挑单5.1.0
    Signed-off-by: 张欣宇 <zhangxinyu74@h-partners.com>
    Change-Id: I55394abaa24ce7ef9204e6261084c631a6206070

--- services/bundlemgr/test/unittest/bms_data_mgr_test/bms_data_mgr_test.cpp
+++ services/bundlemgr/test/unittest/bms_data_mgr_test/bms_data_mgr_test.cpp
@@ -3172,4 +3172,34 @@ HWTEST_F(BmsDataMgrTest, GenerateUuid_0001, TestSize.Level1)
     EXPECT_EQ(uuid5.size(), 36);
     EXPECT_NE(uuid3, uuid5);
 }
+
+/**
+ * @tc.number: SetBundleUserInfoRemovable_0001
+ * @tc.name: SetBundleUserInfoRemovable
+ * @tc.desc: test BundleDataMgr::SetBundleUserInfoRemovable
+ */
+HWTEST_F(BmsDataMgrTest, SetBundleUserInfoRemovable_0001, TestSize.Level1)
+{
+    BundleDataMgr bundleDataMgr;
+    std::string bundleName = "com.test.bundleName";
+    int32_t userId = 100;
+    InnerBundleInfo info;
+    info.baseApplicationInfo_->bundleName = bundleName;
+    InnerBundleUserInfo innerUserInfo;
+    BundleUserInfo userInfo;
+    userInfo.userId = userId;
+    innerUserInfo.bundleUserInfo = userInfo;
+    innerUserInfo.bundleName = bundleName;
+    innerUserInfo.isRemovable = true;
+    info.AddInnerBundleUserInfo(innerUserInfo);
+    bundleDataMgr.bundleInfos_.emplace(bundleName, info);
+    bundleDataMgr.AddUserId(userId);
+
+    bool ret = bundleDataMgr.SetBundleUserInfoRemovable(bundleName, userId, false);
+    EXPECT_TRUE(ret);
+
+    InnerBundleUserInfo getUserInfo;
+    bundleDataMgr.GetInnerBundleUserInfoByUserId(bundleName, userId, getUserInfo);
+    EXPECT_FALSE(getUserInfo.isRemovable);
+}
 } // OHOS
