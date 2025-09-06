e0701fea8b46c84ab750af74b03348ce72bde0bd
Zhou Shihui <zhoushihui4@huawei.com>
2025-09-06 15:26:03 +0800

    解锁优先创建ota新增应用目录
    Signed-off-by: Zhou Shihui <zhoushihui4@huawei.com>

--- services/bundlemgr/include/bundle_data_mgr.h
+++ services/bundlemgr/include/bundle_data_mgr.h
@@ -1175,6 +1175,9 @@ public:
         const int32_t userId, PluginBundleInfo &pluginBundleInfo);
     bool SetBundleUserInfoRemovable(const std::string bundleName, int32_t userId, bool removable);
     ErrCode GetTestRunner(const std::string &bundleName, const std::string &moduleName, ModuleTestRunner &testRunner);
+    void AddOtaNewInstallBundleName(const std::string &bundleName);
+    std::set<std::string> GetOtaNewInstallBundleNames();
+    void ClearOtaNewInstallBundleNames();
 
 private:
     /**
@@ -1406,6 +1409,7 @@ private:
     mutable ffrt::shared_mutex bundleIdMapMutex_;
     mutable ffrt::shared_mutex callbackMutex_;
     mutable ffrt::shared_mutex bundleMutex_;
+    mutable ffrt::shared_mutex otaNewInstallMutex_;
     std::shared_ptr<IBundleDataStorage> dataStorage_;
     std::shared_ptr<IPreInstallDataStorage> preInstallDataStorage_;
     std::shared_ptr<BundleStateStorage> bundleStateStorage_;
@@ -1439,6 +1443,7 @@ private:
     std::set<std::string> appServiceHspBundleName_;
     // using for plugin event callback
     std::vector<sptr<IBundleEventCallback>> pluginCallbackList_;
+    std::set<std::string> otaNewInstallBundleNames_;
 
     static bool HasAppLinkingFlag(uint32_t flags);
 };
