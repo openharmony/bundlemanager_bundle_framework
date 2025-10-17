469be8358a6852ad32f14e15c29c189abf713b61
jerry <ganrui3@huawei.com>
2025-10-17 10:52:26 +0800

    冗余代码删除
    Signed-off-by: jerry <ganrui3@huawei.com>

--- interfaces/inner_api/bundlemgr_extension/src/bms_extension_data_mgr.cpp
+++ interfaces/inner_api/bundlemgr_extension/src/bms_extension_data_mgr.cpp
@@ -618,7 +618,7 @@ bool BmsExtensionDataMgr::DetermineCloneNum(
 
 std::string BmsExtensionDataMgr::GetCompatibleDeviceType(const std::string &bundleName)
 {
-    if ((Init() == ERR_OK) && handler_) {
+    if (Init() == ERR_OK) {
         auto bundleMgrExtPtr =
             BundleMgrExtRegister::GetInstance().GetBundleMgrExt(bmsExtension_.bmsExtensionBundleMgr.extensionName);
         if (bundleMgrExtPtr) {
@@ -741,7 +741,7 @@ ErrCode BmsExtensionDataMgr::RemoveBackupBundleData(const std::string &bundleNam
 ErrCode BmsExtensionDataMgr::BatchGetCompatibleDeviceType(
     const std::vector<std::string> &bundleNames, std::vector<BundleCompatibleDeviceType> &compatibleDeviceTypes)
 {
-    if ((Init() != ERR_OK) || handler_ == nullptr) {
+    if (Init() != ERR_OK) {
         APP_LOGW("link failed");
         return ERR_BUNDLE_MANAGER_EXTENSION_INTERNAL_ERR;
     }
