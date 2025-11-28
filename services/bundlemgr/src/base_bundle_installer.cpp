aaf84c8354fa2734b4629c753218668aa112faa0
lanhaoyu <lanhaoyu3@huawei-partners.com>
2025-11-28 10:46:33 +0800

    add dynamic shortcut
    Signed-off-by: lanhaoyu <lanhaoyu3@huawei-partners.com>

--- services/bundlemgr/src/base_bundle_installer.cpp
+++ services/bundlemgr/src/base_bundle_installer.cpp
@@ -7395,6 +7395,9 @@ ErrCode BaseBundleInstaller::MarkInstallFinish()
                 return ERR_APPEXECFWK_ADD_BUNDLE_ERROR;
             }
         }
+        for (const auto &userInfo : info.GetInnerBundleUserInfos()) {
+            dataMgr_->UpdateShortcutInfoResId(bundleName_, userInfo.second.bundleUserInfo.userId);
+        }
         PrintStartWindowIconId(info);
         return ERR_OK;
     }
