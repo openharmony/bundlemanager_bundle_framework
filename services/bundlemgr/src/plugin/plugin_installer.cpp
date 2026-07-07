82a2d458d4e1376572007c26036f67a9a5de7679
wlh2624_DS <1968860844@qq.com>
2026-07-07 22:09:30 +0800

    fake decompression support dfx Co-Authored-By: Agent Signed-off-by: wlh2624_DS <1968860844@qq.com>

--- services/bundlemgr/src/plugin/plugin_installer.cpp
+++ services/bundlemgr/src/plugin/plugin_installer.cpp
@@ -579,7 +579,8 @@ ErrCode PluginInstaller::ProcessNativeLibrary(
             soPath.c_str(), cpuAbi.c_str(), bundlePath.c_str());
         auto needFakeDecompression =
             newInfo.IsFakeDecompressionEnable() &&
-            BundleUtil::IsSupportFakeDecompression(newInfo.GetBundleName(), newInfo.GetIsKeepAlive());
+            BundleUtil::IsSupportFakeDecompression(newInfo.GetBundleName(), newInfo.GetIsKeepAlive(),
+                newInfo.GetCurModuleName(), newInfo.GetTargetVersion(), newInfo.GetCompatibleVersion());
         auto isSystemApp = newInfo.IsSystemApp();
         auto result = InstalldClient::GetInstance()->ExtractModuleFiles(
             bundlePath, moduleDir, soPath, cpuAbi, needFakeDecompression, isSystemApp);
