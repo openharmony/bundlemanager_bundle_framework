9dfc5edf887cff4cd5943eb99369852115ce861f
daizihan <daizihan@huawei.com>
2026-08-18 16:56:44 +0800

    Add ArkTSRunType
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/11950
    
    Signed-off-by: daizihan <daizihan@huawei.com>
    Change-Id: I42559dc4c8bee2cb5d6e0ac4aacb912d0879dacd

--- interfaces/inner_api/appexecfwk_base/include/bundle_constants.h
+++ interfaces/inner_api/appexecfwk_base/include/bundle_constants.h
@@ -258,6 +258,7 @@ constexpr const char* VERIFY_UNINSTALL_FORCED_VALUE = "true";
 
 constexpr const char* ARKTS_MODE = "arkTSMode";
 constexpr const char* MODULE_ARKTS_MODE = "moduleArkTSMode";
+constexpr const char* ARKTS_RUN_TYPE = "arkTSRunType";
 constexpr const char* AOT_COMPILE_STATUS = "aotCompileStatus";
 constexpr const char* ARKTS_MODE_DYNAMIC = "dynamic";
 constexpr const char* ARKTS_MODE_STATIC = "static";
