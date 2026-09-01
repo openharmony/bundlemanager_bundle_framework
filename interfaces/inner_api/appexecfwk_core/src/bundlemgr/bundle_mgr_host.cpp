4aa7a9b249ec00964fc18a12c1a8ea31f1cf5b4a
heguokai <heguokai6@huawei-partners.com>
2026-09-01 23:15:26 +0800

    update2
    Signed-off-by: heguokai <heguokai6@huawei-partners.com>

--- interfaces/inner_api/appexecfwk_core/src/bundlemgr/bundle_mgr_host.cpp
+++ interfaces/inner_api/appexecfwk_core/src/bundlemgr/bundle_mgr_host.cpp
@@ -3973,7 +3973,7 @@ ErrCode BundleMgrHost::HandleGetAppProvisionInfoInDevice(MessageParcel &data, Me
         APP_LOGE("HandleGetAppProvisionInfoInDevice write failed");
         return ERR_APPEXECFWK_PARCEL_ERROR;
     }
-    if ((ret == ERR_OK) && !reply.WriteParcelable(&appProvisionInfos)) {
+    if ((ret == ERR_OK) && !WriteVectorToParcelIntelligent(appProvisionInfos, reply)) {
         APP_LOGE("write appProvisionInfos failed");
         return ERR_APPEXECFWK_PARCEL_ERROR;
     }
